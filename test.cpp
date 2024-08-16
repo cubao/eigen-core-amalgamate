0cab98284c17d9a4f5812ac5976b29972da95384
ZhiXiong TANG <dvorak4tzx@gmail.com>
2024-08-16 22:40:34 +0800

    test eigen ref/map

--- test.cpp
+++ test.cpp
@@ -3,6 +3,16 @@
 
 #include <Eigen/Core>
 #include <vector>
+#include <iostream>
+
+using RowVectors = Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>;
+using RowVectorsNx3 = RowVectors;
+using RowVectorsNx2 = Eigen::Matrix<double, Eigen::Dynamic, 2, Eigen::RowMajor>;
+
+void update(Eigen::Ref<RowVectorsNx2> xys) {
+	xys.col(0) *= 10;
+	xys.col(1) *= 20;
+}
 
 int main(int argc, char **argv) {
 	Eigen::Vector3d xyz(1, 2, 3);
@@ -15,6 +25,13 @@ int main(int argc, char **argv) {
 	dbg(abcd);
 	Eigen::Map<Eigen::Vector4d>(&abcd[0], 4) *= 10;
 	dbg(abcd);
+
+	auto mat = std::vector<double>{0, 1, 2, 3, 4, 5};
+	Eigen::Map<RowVectorsNx3> M(&mat[0], 2, 3);
+	std::cout << M << std::endl;
+	update(M.leftCols(2));
+	std::cout << M << std::endl;
+	// dbg(M); // https://github.com/sharkdp/dbg-macro/issues/131
 
 	return 0;
 }
