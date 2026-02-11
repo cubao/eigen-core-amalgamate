f3cf8c32415acb15b0dba7f0e1b41673edd0105f
tang zhixiong <zhixiong.tang@momenta.ai>
2026-02-11 15:39:26 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -12,6 +12,27 @@ using RowVectors = Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>;
 using RowVectorsNx3 = RowVectors;
 using RowVectorsNx2 = Eigen::Matrix<double, Eigen::Dynamic, 2, Eigen::RowMajor>;
 
+// Convert quaternion (w, x, y, z) to rotation matrix
+inline Eigen::Matrix3d qwxyz2mat(double w, double x, double y, double z) {
+	Eigen::Matrix3d mat;
+	double w2 = w * w, x2 = x * x, y2 = y * y, z2 = z * z;
+	double xy = x * y, xz = x * z, yz = y * z, wx = w * x, wy = w * y, wz = w * z;
+
+	mat(0, 0) = 1 - 2 * (y2 + z2);
+	mat(0, 1) = 2 * (xy - wz);
+	mat(0, 2) = 2 * (xz + wy);
+
+	mat(1, 0) = 2 * (xy + wz);
+	mat(1, 1) = 1 - 2 * (x2 + z2);
+	mat(1, 2) = 2 * (yz - wx);
+
+	mat(2, 0) = 2 * (xz - wy);
+	mat(2, 1) = 2 * (yz + wx);
+	mat(2, 2) = 1 - 2 * (x2 + y2);
+
+	return mat;
+}
+
 void update(Eigen::Ref<RowVectorsNx2> xys) {
 	xys.col(0) *= 10;
 	xys.col(1) *= 20;
@@ -63,6 +84,20 @@ int main(int argc, char **argv) {
 	Eigen::Quaterniond q3 = q1 * q2;
 	q3.normalize();
 	dbg(q3.coeffs().transpose());
+
+	// Compare Eigen's toRotationMatrix() with qwxyz2mat()
+	std::cout << "\n=== Compare qwxyz2mat vs Eigen ===" << std::endl;
+
+	// Test with q1, q2, q3
+	Eigen::Quaterniond quats[] = {q1, q2, q3};
+	const char* names[] = {"q1", "q2", "q3"};
+	for (int i = 0; i < 3; i++) {
+		double w = quats[i].w(), x = quats[i].x(), y = quats[i].y(), z = quats[i].z();
+		Eigen::Matrix3d eigen_mat = quats[i].toRotationMatrix();
+		Eigen::Matrix3d my_mat = qwxyz2mat(w, x, y, z);
+		double err = (eigen_mat - my_mat).norm();
+		std::cout << names[i] << ": max error = " << err << (err < 1e-14 ? " [PASS]" : " [FAIL]") << std::endl;
+	}
 
 	return 0;
 }
